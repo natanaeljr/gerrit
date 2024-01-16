700d954ae3415a431fbfc3e8736f720105cfe700
Natanael J. Rabello <natanaeljrabello@gmail.com>
2024-01-16 18:10:42 -0300

    Cli struct
    Signed-off-by: Natanael J. Rabello <natanaeljrabello@gmail.com>

---
+++ src/cli.rs
@@ -0,0 +1,311 @@
+use std::cell::RefCell;
+use std::fmt;
+use std::fmt::Display;
+use std::io::{Stdout, Write};
+use std::time::Duration;
+
+use crossterm::cursor::{MoveLeft, MoveToNextLine, MoveUp};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use crossterm::style::{Print, PrintStyledContent, StyledContent, Stylize};
+use crossterm::terminal::{Clear, ClearType, ScrollUp};
+use crossterm::{cursor, event, execute, queue, style, terminal, Command};
+use once_cell::sync::Lazy;
+use parking_lot::ReentrantMutex;
+
+use crate::history::HistoryHandle;
+
+/// Global variable holding CLI data.
+/// It is lazy-initialized on first access.
+/// It is thread-safe and can be locked multiple times in the same thread.
+/// It is RefCell so that the CLI can be mutable and re-assigned.
+static CLI: Lazy<ReentrantMutex<RefCell<CliSingleton>>> =
+    Lazy::new(|| ReentrantMutex::new(RefCell::new(CliSingleton::default())));
+
+/// `CliSingleton` holds global CLI data.
+/// Nothing fancy, just data that should only have once instance
+/// as the CLI is only one per process instance.
+struct CliSingleton {
+    pub prefix: StyledContent<String>,
+    pub symbol: StyledContent<String>,
+}
+
+/// Default initialization of `CliSingleton`
+impl Default for CliSingleton {
+    fn default() -> Self {
+        CliSingleton {
+            prefix: "cli".to_string().stylize(),
+            symbol: ">".to_string().stylize(),
+        }
+    }
+}
+
+/// `cliprint` is just a wrapper macro to be able to print a
+/// str with having to create a Print object before that.
+/// Plus format is expect like print!() macro.
+/// Example:
+/// cliprint!(stdout, "{}", Hello World);
+/// // same as:
+/// execute!(stdout, Print("Hello World");
+#[macro_export]
+macro_rules! cliprint {
+    ($writer:expr, $($arg:tt)*) => {{
+        execute!($writer, Print(format!($($arg)*)));
+    }};
+}
+
+/// Just like `cliprint` but a smart new line is added at the end.
+#[macro_export]
+macro_rules! cliprintln {
+    ($writer:expr) => {
+        execute!($writer, $crate::cli::SmartNewLine(1));
+    };
+    ($writer:expr, $($arg:tt)*) => {{
+        execute!($writer, Print(format!($($arg)*)), $crate::cli::SmartNewLine(1));
+    }};
+}
+
+/// Initialize the terminal for this CLI shell.
+/// This command will configure the terminal to be locked to our shell
+/// thus every input is handled from our application only from this point on
+pub fn initialize() {
+    terminal::enable_raw_mode().unwrap();
+    let mut stdout = std::io::stdout();
+    execute!(stdout, cursor::Show, style::ResetColor).unwrap();
+}
+
+/// Return the terminal to its normal state.
+/// The terminal is unlocked from our application.
+/// Input is handled by the terminal from now on and the attributes are reset.
+/// The CLI shell is finished and the terminal is free.
+pub fn deinitialize() {
+    terminal::disable_raw_mode().unwrap();
+    let mut stdout = std::io::stdout();
+    execute!(stdout, cursor::Show, style::ResetColor).unwrap();
+    stdout.flush().unwrap();
+    /// let terminal commands flush for certain
+    std::thread::sleep(Duration::from_millis(50));
+}
+
+/// Return the stdout object used for CLI
+/// It is centralized here because it can be easier
+/// to change if need in the future.
+pub fn stdout() -> Stdout {
+    std::io::stdout()
+}
+
+/// Update the prompt's prefix string.
+/// Prompt will look like this:
+/// prefix>
+/// where > is the symbol
+pub fn set_prefix(p: StyledContent<String>) {
+    let cli_guard = CLI.lock();
+    let mut cli = cli_guard.borrow_mut();
+    cli.prefix = p;
+}
+
+/// Update the prompt's symbol string.
+/// Prompt will look like this:
+/// prefix>
+/// where > is the symbol
+pub fn set_symbol(s: StyledContent<String>) {
+    let cli_guard = CLI.lock();
+    let mut cli = cli_guard.borrow_mut();
+    cli.symbol = s;
+}
+
+/// Print prompt for user input
+/// This will display the configured `prefix>` in a blank line as a shell prompt.
+pub fn prompt() {
+    let mut stdout = std::io::stdout();
+    let curr_col = crossterm::cursor::position().unwrap().0;
+    if curr_col > 0 {
+        queue!(stdout, SmartNewLine(1), Clear(ClearType::CurrentLine)).unwrap();
+    }
+    let cli_guard = CLI.lock();
+    let mut cli = cli_guard.borrow_mut();
+    execute!(
+        stdout,
+        PrintStyledContent(cli.prefix.clone()),
+        PrintStyledContent(cli.symbol.clone())
+    )
+    .unwrap();
+}
+
+pub fn smart_new_line(num: u16) -> MoveToNextLine {
+    let mut stdout = std::io::stdout();
+    let curr_row = crossterm::cursor::position().unwrap().1;
+    let term_max_row = crossterm::terminal::size().unwrap().1 - 1;
+    if curr_row == term_max_row {
+        execute!(stdout, ScrollUp(num), MoveUp(num)).unwrap();
+    }
+    MoveToNextLine(num)
+}
+
+/// Check if we are at the last row in the terminal,
+/// then we may need to scroll up because we are in RAW mode,
+/// and the terminal won't do that automatically in this mode.
+/// This Command quietly does that before `MoveToNextLine`.
+/// Then return the new line object, so this function can be used inside
+/// execute! or queue! in place of the actual `MoveToNextLine` object.
+pub struct SmartNewLine(pub u16);
+
+/// Implementation of the SmartNewLine that handles next-line + scroll.
+impl Command for SmartNewLine {
+    fn write_ansi(&self, f: &mut impl fmt::Write) -> fmt::Result {
+        let curr_row = crossterm::cursor::position().unwrap().1;
+        let term_max_row = crossterm::terminal::size().unwrap().1 - 1;
+        if curr_row == term_max_row {
+            ScrollUp(self.0).write_ansi(f)?;
+            MoveUp(self.0).write_ansi(f)?;
+        }
+        MoveToNextLine(self.0).write_ansi(f)?;
+        Ok(())
+    }
+
+    // #[cfg(windows)]
+    // fn execute_winapi(&self) -> std::io::Result<()> {
+    //     if self.0 != 0 {
+    //         sys::move_to_previous_line(self.0)?;
+    //     }
+    //     Ok(())
+    // }
+}
+
+/// Read input from terminal until enter is given.
+/// Returns the entered characters until '\n'.
+/// This is a fully featured prompt handling with text manipulation
+/// just like a shell, with history, arrows handling, backspace, alt, ctrl, etc.
+pub fn read_inputln() -> std::io::Result<String> {
+    let mut stdout = stdout();
+    let mut history = HistoryHandle::get();
+    let mut prompt = String::new();
+    let mut last_prompt: Option<String> = None;
+    loop {
+        match event::read() {
+            // BACKSPACE
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Backspace,
+                kind: KeyEventKind::Press,
+                modifiers,
+                state: _,
+            })) => {
+                if !prompt.is_empty() {
+                    let count: u16;
+                    if modifiers == KeyModifiers::ALT {
+                        if let Some(idx) = prompt.rfind(" ") {
+                            // TODO: fix line wrap and overflow
+                            count = (prompt.len() - idx) as u16;
+                            _ = prompt.split_off(idx);
+                        } else {
+                            count = prompt.len() as u16;
+                            prompt.clear();
+                        }
+                    } else {
+                        prompt.pop();
+                        count = 1;
+                    }
+                    execute!(stdout, MoveLeft(count), Clear(ClearType::UntilNewLine)).unwrap();
+                }
+            }
+            // ENTER
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Enter,
+                kind: KeyEventKind::Press,
+                modifiers: _,
+                state: _,
+            })) => {
+                if !prompt.is_empty() {
+                    history.add(prompt.clone());
+                }
+                return Ok(prompt);
+            }
+            // CTRL + C
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char('c'),
+                kind: KeyEventKind::Press,
+                modifiers: KeyModifiers::CONTROL,
+                state: _,
+            })) => {
+                execute!(stdout, Print("^C")).unwrap();
+                return Ok(String::from("quit"));
+            }
+            // CTRL + D
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char('d'),
+                kind: KeyEventKind::Press,
+                modifiers: KeyModifiers::CONTROL,
+                state: _,
+            })) => {
+                execute!(stdout, Print("^D")).unwrap();
+                return Ok(String::from("quit"));
+            }
+            // CTRL + L
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char('l'),
+                kind: KeyEventKind::Press,
+                modifiers: KeyModifiers::CONTROL,
+                state: _,
+            })) => {
+                let curr_row = crossterm::cursor::position().unwrap().1;
+                execute!(stdout, ScrollUp(curr_row), MoveUp(curr_row)).unwrap()
+            }
+            // ARROW UP
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Up,
+                kind: KeyEventKind::Press,
+                modifiers: _,
+                state: _,
+            })) => {
+                if let Some(up_next) = history.up_next() {
+                    let count = prompt.len() as u16;
+                    if last_prompt == None {
+                        last_prompt = Some(prompt.clone())
+                    }
+                    prompt = up_next;
+                    if count > 0 {
+                        execute!(stdout, MoveLeft(count), Clear(ClearType::UntilNewLine),).unwrap();
+                    }
+                    execute!(stdout, Print(prompt.as_str())).unwrap();
+                }
+            }
+            // ARROW DOWN
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Down,
+                kind: KeyEventKind::Press,
+                modifiers: _,
+                state: _,
+            })) => {
+                if let Some(down_next) = history.down_next() {
+                    let count = prompt.len() as u16;
+                    prompt = down_next;
+                    if count > 0 {
+                        execute!(stdout, MoveLeft(count), Clear(ClearType::UntilNewLine)).unwrap();
+                    }
+                    execute!(stdout, Print(prompt.as_str())).unwrap();
+                } else {
+                    let count = prompt.len() as u16;
+                    if count > 0 {
+                        execute!(stdout, MoveLeft(count), Clear(ClearType::UntilNewLine),).unwrap();
+                    }
+                    if last_prompt.is_some() {
+                        prompt = last_prompt.unwrap();
+                        last_prompt = None;
+                    }
+                    execute!(stdout, Print(prompt.as_str())).unwrap();
+                }
+            }
+            // CHARACTERS
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char(c),
+                kind: KeyEventKind::Press,
+                modifiers: _,
+                state: _,
+            })) => {
+                execute!(stdout, Print(c)).unwrap();
+                prompt.push(c);
+            }
+            // ANYTHING
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use std::io::{stdout, Write};
-use std::thread::sleep;
-use std::time::Duration;
 
-use crossterm::cursor::{MoveLeft, MoveToNextLine, MoveUp};
-use crossterm::event;
-use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
-use crossterm::style::{Color, Print, PrintStyledContent, Stylize};
-use crossterm::terminal::{Clear, ClearType, ScrollUp};
-use crossterm::{execute, terminal};
+use crossterm::execute;
+use crossterm::style::{Print, PrintStyledContent, Stylize};
 
-use history::HistoryHandle;
+use crate::cli::smart_new_line;
 
 mod cli;
 mod history;
@@ -45,229 +39,82 @@ mod history;
 /// - [x] HISTORY up/down with on-going command restore on last down-arrow
 /// - [ ] Handle left/right arrows and prompt in-middle insert characters,
 ///       prompt will have to shift the characters.
+/// - [ ] Trim whitespace from user input text
 ///
 fn main() -> std::io::Result<()> {
-    let mut stdout = stdout();
+    cli::initialize();
+    cli::set_prefix("gerrit".to_string().stylize());
+    cli::set_symbol(">".to_string().green());
+    let mut stdout = cli::stdout();
+    cliprintln!(stdout, "Gerrit command-line interface");
+
     let mut quit = false;
-
-    terminal::enable_raw_mode()?;
-    execute!(
-        stdout,
-        Print("Gerrit command-line interface"),
-        smart_new_line(1)
-    )
-    .unwrap();
-
     while !quit {
-        print_gerrit_prefix(&mut stdout);
-
-        match read_until_newline(&mut stdout)?.as_str() {
-            "help" | "?" => execute!(
-                stdout,
-                smart_new_line(1),
-                Print(" help"),
-                smart_new_line(1),
-                Print(" remote"),
-                smart_new_line(1),
-                Print(" quit"),
-                smart_new_line(2),
-            )
-            .unwrap(),
-            "remote" => execute!(
-                stdout,
-                smart_new_line(1),
-                Print("remote one"),
-                smart_new_line(1),
-                Print("remote two"),
-                smart_new_line(2),
-            )
-            .unwrap(),
+        cli::prompt();
+        let input = cli::read_inputln()?;
+        match input.as_str() {
             "quit" | "exit" => quit = true,
-            str if !str.is_empty() => {
-                execute!(
-                    stdout,
-                    smart_new_line(1),
-                    PrintStyledContent("x".with(Color::Red)),
-                    Print(" Unknown command"),
-                    smart_new_line(1)
-                )
-                .unwrap();
+            "help" | "?" => print_help(),
+            "remote" => cmd_remote(),
+            any if !any.is_empty() => {
+                print_unknown_command();
             }
-            _ => {
-                execute!(stdout, smart_new_line(1)).unwrap();
-            }
-        };
+            _ => {}
+        }
     }
+    print_done();
+    cli::deinitialize();
+    Ok(())
+}
 
+pub fn print_help() {
+    let mut stdout = stdout();
     execute!(
         stdout,
         smart_new_line(1),
-        PrintStyledContent("✓".with(Color::Green)),
+        Print(" help"),
+        smart_new_line(1),
+        Print(" remote"),
+        smart_new_line(1),
+        Print(" quit"),
+        smart_new_line(2),
+    )
+    .unwrap()
+}
+
+pub fn print_unknown_command() {
+    let mut stdout = stdout();
+    execute!(
+        stdout,
+        smart_new_line(1),
+        PrintStyledContent("x".red()),
+        Print(" Unknown command"),
+        smart_new_line(1)
+    )
+    .unwrap();
+}
+
+pub fn cmd_remote() {
+    let mut stdout = stdout();
+    execute!(
+        stdout,
+        smart_new_line(1),
+        Print("remote one"),
+        smart_new_line(1),
+        Print("remote two"),
+        smart_new_line(2),
+    )
+    .unwrap()
+}
+
+pub fn print_done() {
+    let mut stdout = stdout();
+    execute!(
+        stdout,
+        smart_new_line(1),
+        PrintStyledContent("✓".green()),
         Print(" Done"),
         smart_new_line(1)
     )
     .unwrap();
-    terminal::disable_raw_mode()?;
-    stdout.flush()?;
-    sleep(Duration::from_millis(50));
-    Ok(())
-}
-
-/// Check if we are at the last row in the terminal,
-/// then we may need to scroll up because we are in RAW mode,
-/// and the terminal won't do that automatically in this mode.
-/// This function quietly does that before `MoveToNextLine`.
-/// Then return the new line object, so this function can be used inside
-/// execute! or queue! in place of the actual `MoveToNextLine` object.
-pub fn smart_new_line(num: u16) -> MoveToNextLine {
-    let mut stdout = stdout();
-    let curr_row = crossterm::cursor::position().unwrap().1;
-    let term_max_row = crossterm::terminal::size().unwrap().1 - 1;
-    if curr_row == term_max_row {
-        execute!(stdout, ScrollUp(num), MoveUp(num)).unwrap();
-    }
-    MoveToNextLine(num)
-}
-
-pub fn print_gerrit_prefix<W: Write>(stdout: &mut W) {
-    execute!(
-        stdout,
-        Print("gerrit"),
-        PrintStyledContent("> ".with(Color::Green)),
-    )
-    .unwrap();
-}
-
-pub fn read_until_newline<W: Write>(stdout: &mut W) -> std::io::Result<String> {
-    let mut history = HistoryHandle::get();
-    let mut prompt = String::new();
-    let mut last_prompt: Option<String> = None;
-    loop {
-        match event::read() {
-            // BACKSPACE
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Backspace,
-                kind: KeyEventKind::Press,
-                modifiers,
-                state: _,
-            })) => {
-                if !prompt.is_empty() {
-                    let count: u16;
-                    if modifiers == KeyModifiers::ALT {
-                        if let Some(idx) = prompt.rfind(" ") {
-                            // TODO: fix line wrap and overflow
-                            count = (prompt.len() - idx) as u16;
-                            _ = prompt.split_off(idx);
-                        } else {
-                            count = prompt.len() as u16;
-                            prompt.clear();
-                        }
-                    } else {
-                        prompt.pop();
-                        count = 1;
-                    }
-                    execute!(stdout, MoveLeft(count), Clear(ClearType::UntilNewLine)).unwrap();
-                }
-            }
-            // ENTER
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Enter,
-                kind: KeyEventKind::Press,
-                modifiers: _,
-                state: _,
-            })) => {
-                if !prompt.is_empty() {
-                    history.add(prompt.clone());
-                }
-                return Ok(prompt);
-            }
-            // CTRL + C
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Char('c'),
-                kind: KeyEventKind::Press,
-                modifiers: KeyModifiers::CONTROL,
-                state: _,
-            })) => {
-                execute!(stdout, Print("^C")).unwrap();
-                return Ok(String::from("quit"));
-            }
-            // CTRL + D
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Char('d'),
-                kind: KeyEventKind::Press,
-                modifiers: KeyModifiers::CONTROL,
-                state: _,
-            })) => {
-                execute!(stdout, Print("^D")).unwrap();
-                return Ok(String::from("quit"));
-            }
-            // CTRL + L
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Char('l'),
-                kind: KeyEventKind::Press,
-                modifiers: KeyModifiers::CONTROL,
-                state: _,
-            })) => {
-                let curr_row = crossterm::cursor::position().unwrap().1;
-                execute!(stdout, ScrollUp(curr_row), MoveUp(curr_row)).unwrap()
-            }
-            // ARROW UP
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Up,
-                kind: KeyEventKind::Press,
-                modifiers: _,
-                state: _,
-            })) => {
-                if let Some(up_next) = history.up_next() {
-                    let count = prompt.len() as u16;
-                    if last_prompt == None {
-                        last_prompt = Some(prompt.clone())
-                    }
-                    prompt = up_next;
-                    if count > 0 {
-                        execute!(stdout, MoveLeft(count), Clear(ClearType::UntilNewLine),).unwrap();
-                    }
-                    execute!(stdout, Print(prompt.as_str())).unwrap();
-                }
-            }
-            // ARROW DOWN
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Down,
-                kind: KeyEventKind::Press,
-                modifiers: _,
-                state: _,
-            })) => {
-                if let Some(down_next) = history.down_next() {
-                    let count = prompt.len() as u16;
-                    prompt = down_next;
-                    if count > 0 {
-                        execute!(stdout, MoveLeft(count), Clear(ClearType::UntilNewLine)).unwrap();
-                    }
-                    execute!(stdout, Print(prompt.as_str())).unwrap();
-                } else {
-                    let count = prompt.len() as u16;
-                    if count > 0 {
-                        execute!(stdout, MoveLeft(count), Clear(ClearType::UntilNewLine),).unwrap();
-                    }
-                    if last_prompt.is_some() {
-                        prompt = last_prompt.unwrap();
-                        last_prompt = None;
-                    }
-                    execute!(stdout, Print(prompt.as_str())).unwrap();
-                }
-            }
-            // CHARACTERS
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Char(c),
-                kind: KeyEventKind::Press,
-                modifiers: _,
-                state: _,
-            })) => {
-                execute!(stdout, Print(c)).unwrap();
-                prompt.push(c);
-            }
-            // ANYTHING
-            _ => {}
-        }
-    }
 }
